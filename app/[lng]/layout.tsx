3324b6f9d8f31f721e248df9ebb72db748a26094
salas <salas@debian-BULLSEYE-live-builder-AMD64>
2023-06-13 13:30:58 +0300

    test: android firefox line-height

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Metadata } from "next";
 import { languages } from "../i18n/settings";
+import Head from "next/head";
 
 export async function generateStaticParams() {
   return languages.map((lng) => ({ lng }));
@@ -26,6 +27,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang={lng}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0"
+        ></meta>
+      </Head>
       <body>{children}</body>
     </html>
   );
