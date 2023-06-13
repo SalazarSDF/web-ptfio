06ccb2e3e247b91d354b3f99fab3714bbf6c3d48
salas <salas@debian-BULLSEYE-live-builder-AMD64>
2023-06-13 18:08:05 +0300

    add: text for about page

--- app/[lng]/about/page.tsx
+++ app/[lng]/about/page.tsx
@@ -1,15 +1,41 @@
 import Link from "next/link";
 import { useTranslation } from "@/app/i18n";
+import styles from "./about.module.css";
+
+const listItems = [
+  { name: "home", href: "/" },
+  { name: "about", href: "/about" },
+  { name: "projects", href: "/projects" },
+];
+
 export default async function About({
   params: { lng },
 }: {
   params: { lng: string };
 }) {
   const { t } = await useTranslation(lng, "about-page");
+  const aboutInfo = t("about-info").split("|");
   return (
-    <>
+    <div className={styles.aboutContainer}>
+      <header className={styles.header}>
+        <nav>
+          <ul className={styles.navList}>
+            {listItems.map((el) => (
+              <li key={el.name}>
+                <Link href={`${lng}/${el.href}`}>{t(`${el.name}`)}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+
       <h1>{t("title")}</h1>
       <Link href={`/${lng}`}>Back</Link>
-    </>
+      <div className={styles.parList}>
+        {aboutInfo.map((el) => (
+          <p key={el}>{el}</p>
+        ))}
+      </div>
+    </div>
   );
 }
