cca3eb32b4275377e20925bb69452f880fe974fe
salas <salas@debian-BULLSEYE-live-builder-AMD64>
2023-06-20 18:07:14 +0300

    add: projects to projects page

--- app/[lng]/projects/page.tsx
+++ app/[lng]/projects/page.tsx
@@ -1,8 +1,9 @@
 import styles from "./projects.module.css";
 import { useTranslation } from "@/app/i18n";
 import NavAside from "@/app/componets/NavAside";
-import { FaJsSquare } from "react-icons/fa";
+import { FaJsSquare, FaReact, FaGamepad } from "react-icons/fa";
 import { BsArrowUpRight } from "react-icons/bs";
+import { SiNextdotjs } from "react-icons/si";
 
 const projectsArr = [
   {
@@ -15,31 +16,43 @@ const projectsArr = [
     techTask:
       "https://github.com/rolling-scopes-school/tasks/tree/master/stage1/stream2/online-zoo#%D0%BD%D0%B5%D0%B4%D0%B5%D0%BB%D1%8F-3",
     gitLink: "https://github.com/SalazarSDF/online-zoo_layout-",
+    techStack: "js",
   },
   {
     name: "Song Bird",
     date: "2022-11-22",
-    translateKey: "song-bird",
     link: "https://rolling-scopes-school.github.io/salazarsdf-JSFE2022Q3/song-bird/",
+    translateKey: "song-bird",
     techTask:
       "https://github.com/rolling-scopes-school/tasks/blob/master/tasks/songbird/songbird-2022q3.md",
+    techStack: ["js", "game"],
   },
   {
     name: "Gem Puzzle",
     date: "2022-10-18",
-    translateKey: "gem-puzzle",
     link: "https://rolling-scopes-school.github.io/salazarsdf-JSFE2022Q3/puzzle-game/",
+    translateKey: "gem-puzzle",
     techTask:
       "https://github.com/rolling-scopes-school/tasks/blob/master/tasks/stage-1/dom-api/codejam-the-gem-puzzle.md",
+    techStack: ["js", "game"],
   },
-
   {
     name: "React Online Store",
     date: "2023-03-29",
+    link: "https://scintillating-mochi-031dfe.netlify.app/",
     translateKey: "react-online-store",
-    link: "https://github.com/rolling-scopes-school/tasks/tree/master/tasks/online-store-team",
     techTask:
       "https://github.com/rolling-scopes-school/tasks/tree/master/tasks/online-store-team",
+    gitLink: "https://github.com/SalazarSDF/React_Online-Store",
+    techStack: "react",
+  },
+  {
+    name: "Portfolio",
+    date: "2023-06-08",
+    link: "https://github.com/SalazarSDF/web-ptfio",
+    translateKey: "portfolio",
+    gitLink: "https://github.com/SalazarSDF/web-ptfio",
+    techStack: ["react", "next"],
   },
 ];
 
@@ -47,50 +60,79 @@ type project = {
   name: string;
   date: string;
   link: string;
-  techTask: string;
+  translateKey: string;
+  techTask?: string;
   figmaLink?: string;
   gitLink?: string;
+  techStack: string | string[];
 };
 
-function JsProjects2({
+function Projects({
   project,
   projectDescr,
 }: {
   project: project;
   projectDescr: string;
 }) {
+  const [_, month, day, year] = new Date(project.date).toString().split(" ");
   return (
     <li className={styles.li}>
-      <h3 className={styles.projectHeading}>{project.name}</h3>
-      <hr className={styles.hor} />
-      <a className={styles.project} href={project.link}>
+      {/* <h3 className={styles.projectHeading}>{project.name}</h3> */}
+      {/* <hr className={styles.hor} /> */}
+      <a target="_blank" className={styles.project} href={project.link}>
         <time
           className={styles.date}
           dateTime={`${project.date}T00:00:00.000Z`}
         >
-          {project.date}
+          {`${month} ${day}, ${year}`}
         </time>
         <h4 className={styles.projectName}>{project.name}</h4>
         <p className={styles.projectDescr}>{projectDescr}</p>
         <div className={styles.svgContainer}>
-          <FaJsSquare className={styles.projectSvg} />
+          {project.techStack.includes("js") && (
+            <FaJsSquare className={styles.jsSvg} />
+          )}
+          {project.techStack.includes("game") && (
+            <FaGamepad className={styles.gameSvg} />
+          )}
+          {project.techStack.includes("react") && (
+            <FaReact className={styles.reactSvg} />
+          )}
+          {project.techStack.includes("next") && (
+            <SiNextdotjs className={styles.nextSvg} />
+          )}
+
           <BsArrowUpRight className={styles.projectArrow} />
         </div>
       </a>
 
       <div className={styles.aditionLinks}>
-        <a className={styles.aditionLink} href={project.techTask}>
-          Tech
-          <BsArrowUpRight />
-        </a>
+        {project.techTask && (
+          <a
+            target="_blank"
+            className={styles.aditionLink}
+            href={project.techTask}
+          >
+            Tech
+            <BsArrowUpRight />
+          </a>
+        )}
         {project.figmaLink && (
-          <a className={styles.aditionLink} href={project.figmaLink}>
+          <a
+            target="_blank"
+            className={styles.aditionLink}
+            href={project.figmaLink}
+          >
             Figma
             <BsArrowUpRight />
           </a>
         )}
         {project.gitLink && (
-          <a className={styles.aditionLink} href={project.gitLink}>
+          <a
+            target="_blank"
+            className={styles.aditionLink}
+            href={project.gitLink}
+          >
             Git Hub
             <BsArrowUpRight />
           </a>
@@ -100,58 +142,7 @@ function JsProjects2({
   );
 }
 
-function JsProjects() {
-  return (
-    <li className={styles.li}>
-      <h3 className={styles.projectHeading}>Online Zoo</h3>
-      <hr className={styles.hor} />
-      <a
-        className={styles.project}
-        href="https://salazarsdf.github.io/online-zoo_layout-/dist/"
-      >
-        <time className={styles.date} dateTime="2022-09-28T00:00:00.000Z">
-          Sep 28, 2022
-        </time>
-        <h4 className={styles.projectName}>Online Zoo</h4>
-        <p className={styles.projectDescr}>
-          Learn css and DOM-tree, mostly interesting part was implement slider
-          and randomly shuffle cards.
-        </p>
-        <div className={styles.svgContainer}>
-          <FaJsSquare className={styles.projectSvg} />
-          <BsArrowUpRight className={styles.projectArrow} />
-        </div>
-      </a>
-
-      <div className={styles.aditionLinks}>
-        <a
-          className={styles.aditionLink}
-          href="https://github.com/rolling-scopes-school/tasks/blob/master/tasks/stage-1/dom-api/codejam-the-gem-puzzle.md"
-        >
-          Technical task
-          <BsArrowUpRight />
-        </a>
-
-        <a
-          className={styles.aditionLink}
-          href="https://github.com/rolling-scopes-school/tasks/blob/master/tasks/stage-1/dom-api/codejam-the-gem-puzzle.md"
-        >
-          Technical task
-          <BsArrowUpRight />
-        </a>
-        <a
-          className={styles.aditionLink}
-          href="https://github.com/rolling-scopes-school/tasks/blob/master/tasks/stage-1/dom-api/codejam-the-gem-puzzle.md"
-        >
-          Technical task
-          <BsArrowUpRight />
-        </a>
-      </div>
-    </li>
-  );
-}
-
-export default async function Projects({
+export default async function ProjectsPage({
   params: { lng },
 }: {
   params: { lng: string };
@@ -165,10 +156,9 @@ export default async function Projects({
         <p className={styles.par}>{t("description")}</p>
         <hr className={styles.hor} />
         <section>
-          {/* <ul className={styles.projectsList}>{JsProjects()}</ul> */}
           <ul className={styles.projectsList}>
             {projectsArr.map((el) => (
-              <JsProjects2
+              <Projects
                 key={el.link}
                 project={el}
                 projectDescr={t(el.translateKey)}
